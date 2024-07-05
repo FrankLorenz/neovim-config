40f89a4a924403846b35574ff2b21d61c89fcc6a
Frank Lorenz <frank.lorenz@riedel.net>
2024-07-05 17:34:21 +0200

    Deactivate useless C-Z key binding for autocomplete trigger

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,7 +5,7 @@ return {
       table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-        ["<C-Z>"] = cmp.mapping.confirm({ select = true }),
+        -- ["<C-Z>"] = cmp.mapping.confirm({ select = true }),
         ["<CR>"] = cmp.config.disable,
       })
     end,
