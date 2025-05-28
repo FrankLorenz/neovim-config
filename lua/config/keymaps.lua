0d8286eebb3111a19bfa9adcebb4f5461e5c2138
Frank Lorenz <frank.lorenz@riedel.net>
2025-05-28 17:46:35 +0200

    Add blink-cmp-copilot; add mark, copy and paste with usual windows keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,20 @@ local Util = require("lazyvim.util")
 -- use `vim.keymap.set` instead
 local map = Util.safe_keymap_set
 
+-- Copy selected text to system clipboard with Ctrl-C in visual mode
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
+
+-- Paste from system clipboard with Ctrl-V in normal and visual mode
+vim.keymap.set({'n', 'v'}, '<C-v>', '"+p', { noremap = true, silent = true })
+
+--   Shift + Arrow keys to enter visual mode and expand selection
+vim.keymap.set('n', '<S-Left>',  'v<Left>',  { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Right>', 'v<Right>', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Up>',    'v<Up>',    { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Down>',  'v<Down>',  { noremap = true, silent = true })
+
+vim.keymap.set('v', '<S-Left>',  '<Left>',   { noremap = true, silent = true })
+vim.keymap.set('v', '<S-Right>', '<Right>',  { noremap = true, silent = true })
+vim.keymap.set('v', '<S-Up>',    '<Up>',     { noremap = true, silent = true })
+vim.keymap.set('v', '<S-Down>',  '<Down>',   { noremap = true, silent = true })
+
